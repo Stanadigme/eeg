b9e3ec4937d99456172f8a9071946aa0899a88a5
Stanadigme <contact@p0si.net>
2021-09-09 11:18:29 -0400

    Pi

--- launch.sh
+++ launch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-DISPLAY=:0 /home/pi/processing/processing-3.5.3/processing-java --sketch=/home/pi/eeg/Interfaces --present 0
+DISPLAY=:0 /home/pi/processing/processing-3.5.3/processing-java --sketch=/home/pi/eeg/Interfaces --present 2
